1dabf6b401556153f57a6af020cda61234c1e6b0
Stanley Wintergreen <kepocnhh@gmail.com>
2023-02-17 15:03:18 +0300

    init gradle

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+buildscript {
+    repositories.mavenCentral()
+
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.21")
+    }
+}
+
+task<Delete>("clean") {
+    delete = setOf(buildDir)
+}
