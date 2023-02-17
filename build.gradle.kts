7e5ff8e97a03f15eb5c42997c385acae22eab05b
Stanley Wintergreen <kepocnhh@gmail.com>
2023-02-17 15:06:12 +0300

    init buildSrc

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,10 @@ buildscript {
     repositories.mavenCentral()
 
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.21")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Version.kotlin}")
     }
 }
 
 task<Delete>("clean") {
-    delete = setOf(buildDir)
+    delete = setOf(buildDir, "buildSrc/build")
 }
