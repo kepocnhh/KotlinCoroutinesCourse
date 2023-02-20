ae178cfc2c7586723a72b04b305b437319b9ab60
Stanley Wintergreen <kepocnhh@gmail.com>
2023-02-20 23:35:09 +0300

    lesson/02: cancel

--- lib/src/main/kotlin/kt/coroutines/course/lessons/lesson_02/Lesson02.kt
+++ lib/src/main/kotlin/kt/coroutines/course/lessons/lesson_02/Lesson02.kt
@@ -1,13 +1,18 @@
 package kt.coroutines.course.lessons.lesson_02
 
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
 
 object Lesson02 {
     fun learn() {
         println("\n\t---")
         learnJob()
+        println("\n\t---")
+        learnCancel()
     }
 }
 
@@ -30,5 +35,28 @@ private fun learnJob() {
                                 [cancelling] -> [cancelled]
             """.trimIndent()
         )
+    }
+}
+
+private fun learnCancel() {
+    runBlocking {
+        println("launch job...")
+        val job = launch {
+            println("job launched")
+            val size = 1_000
+            repeat(1_000) { index ->
+                println("job: $index/$size")
+                delay(1500.milliseconds)
+            }
+        }
+        println("start delay")
+        delay(4.seconds)
+        println("finish delay")
+        println("cancel job: $job")
+        job.cancel()
+        println("join job: $job")
+        job.join()
+        println("cancelled job: $job")
+//        job.cancelAndJoin()
     }
 }
