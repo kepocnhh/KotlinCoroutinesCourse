77c2dc4fe3051024da111fd67dcf60f991c0cf6a
Stanley Wintergreen <kepocnhh@gmail.com>
2023-02-20 23:46:19 +0300

    lesson/02: cancellation

--- lib/src/main/kotlin/kt/coroutines/course/lessons/lesson_02/Lesson02.kt
+++ lib/src/main/kotlin/kt/coroutines/course/lessons/lesson_02/Lesson02.kt
@@ -1,10 +1,15 @@
 package kt.coroutines.course.lessons.lesson_02
 
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlin.coroutines.CoroutineContext
 import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.nanoseconds
 import kotlin.time.Duration.Companion.seconds
 
 object Lesson02 {
@@ -13,6 +18,8 @@ object Lesson02 {
         learnJob()
         println("\n\t---")
         learnCancel()
+        println("\n\t---")
+        learnCancellation()
     }
 }
 
@@ -46,7 +53,7 @@ private fun learnCancel() {
             val size = 1_000
             repeat(1_000) { index ->
                 println("job: $index/$size")
-                delay(1500.milliseconds)
+                delay(1.5.seconds)
             }
         }
         println("start delay")
@@ -57,6 +64,32 @@ private fun learnCancel() {
         println("join job: $job")
         job.join()
         println("cancelled job: $job")
-//        job.cancelAndJoin()
+    }
+}
+
+private fun learnCancellation() {
+    runBlocking {
+        println("launch job...")
+        val context: CoroutineContext = Dispatchers.Default
+        val job = launch(context) {
+            println("job launched")
+            println("no suspend functions")
+            println("context: $context")
+            var index = 0
+            while (isActive) {
+                println("job: ${index++}")
+                val now = System.nanoTime().nanoseconds
+                while (System.nanoTime().nanoseconds - now < 1.5.seconds) {
+                    // ...
+                }
+            }
+            println("job is not active")
+        }
+        println("start delay")
+        delay(4.seconds)
+        println("finish delay")
+        println("cancel and join job: $job")
+        job.cancelAndJoin()
+        println("cancelled and joined job: $job")
     }
 }
